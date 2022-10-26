9a366bb5ff824e426ccb3dd563c70c097661d0af
Theo Korbosli <theo.korbosli@hotmail.com>
2022-10-26 13:48:50 +0200

    Phase 2 : Premier test "Hello world!"

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController {
+    
+    /**
+     * @Route("/", name="accueil")
+     * @return Response
+     */
+    public function index(): Response {
+        return new Response('Hello world!');
+    }
+}
