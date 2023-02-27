7e30906a4e1640e14f4fe6391e565f21c67f4049
Theo Korbosli <theo.korbosli@hotmail.com>
2023-02-27 20:43:01 +0100

    Phase 19 : gestion des images

--- src/Form/VisiteType.php
+++ src/Form/VisiteType.php
@@ -8,6 +8,7 @@ use DateTime;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -34,10 +35,14 @@ class VisiteType extends AbstractType
                 'label' => 't° max'
             ])
             ->add('environnements', EntityType::class, [
-                'class' => \App\Entity\Environnement::class,
+                'class' => Environnement::class,
                 'choice_label' => 'nom',
                 'multiple' => true,
                 'required' => false
+            ])
+            ->add('imageFile', FileType::class, [
+                'required' => false,
+                'label' => 'sélection image'
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Enregistrer'
